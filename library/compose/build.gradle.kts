6578743f1365ca29e696e4b9feadef8f76402018
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-12-29 02:16:55 +0900

    chore: fix version, add binary compat plugin

--- library/compose/build.gradle.kts
+++ library/compose/build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 l2hyunwoo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -7,6 +22,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
     alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.binaryCompatibilityValidator)
 }
 
 group = "io.github.l2hyunwoo.camera.compose"
