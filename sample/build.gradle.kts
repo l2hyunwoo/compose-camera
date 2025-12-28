7ccdade2292c98c6566e67b7164b9463a3c04ef4
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-12-29 01:43:11 +0900

    chore: add icon deps

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -51,6 +51,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
+            implementation(compose.materialIconsExtended)
         }
 
         androidMain.dependencies {
