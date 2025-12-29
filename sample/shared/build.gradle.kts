0300933eb89633612b6fc67a29a9f8bc2831f8a0
hanminjae <hanminjae131@gmail.com>
2025-12-29 16:18:11 +0900

    style: apply spotless formatting

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 l2hyunwoo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -12,7 +27,7 @@ kotlin {
         namespace = "io.github.l2hyunwoo.compose.camera.sample.shared"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
-        
+
         compilations.all {
             compileTaskProvider.configure {
                 compilerOptions {
