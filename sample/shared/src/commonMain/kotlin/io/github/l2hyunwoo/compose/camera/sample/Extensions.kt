0c8addc53b48deec5fe72127a4ad50714c914494
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-04 02:47:39 +0900

    fix: ios build

---
+++ sample/shared/src/commonMain/kotlin/io/github/l2hyunwoo/compose/camera/sample/Extensions.kt
@@ -0,0 +1,5 @@
+package io.github.l2hyunwoo.compose.camera.sample
+
+import androidx.compose.ui.Modifier
+
+expect fun Modifier.applyTestTagsAsResourceId(): Modifier
---
+++ sample/shared/src/iosMain/kotlin/io/github/l2hyunwoo/compose/camera/sample/Extensions.ios.kt
@@ -0,0 +1,7 @@
+package io.github.l2hyunwoo.compose.camera.sample
+
+import androidx.compose.ui.Modifier
+
+actual fun Modifier.applyTestTagsAsResourceId(): Modifier {
+    return this
+}
