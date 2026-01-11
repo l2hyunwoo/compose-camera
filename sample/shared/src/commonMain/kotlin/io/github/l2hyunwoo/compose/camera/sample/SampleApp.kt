38e74b878fe6b011a379093926d459af78f2b13e
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-11 14:07:49 +0900

    fix: apply code review

--- sample/shared/src/commonMain/kotlin/io/github/l2hyunwoo/compose/camera/sample/SampleApp.kt
+++ sample/shared/src/commonMain/kotlin/io/github/l2hyunwoo/compose/camera/sample/SampleApp.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavType
